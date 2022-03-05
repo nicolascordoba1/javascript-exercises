bfc2905a5921b7db4d4acd5737d5e16d10aeb89f
Nicolas Cordoba <nicolascordoba97@hotmail.com>
2022-03-05 14:39:43 -0500

    First day resolving the TOP exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = string.split(",")
+    reverse = reverse.join(',')
+    let newText = ''
+    for(i=string.length-1; i>=0;i--){
+        newText += reverse[i]
+
+    }
+    return newText
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(argumentos) {
+    let lista = []
+    for(let i=0; i<argumentos[0].length;i++){
+        for (let j=1;j<i;j++){
+          if(lista[i] === argumentos[j]){
+            lista.splice(i,1)
+            }  
+        }      
+    }
+    return lista
 };
 
 // Do not edit below this line
