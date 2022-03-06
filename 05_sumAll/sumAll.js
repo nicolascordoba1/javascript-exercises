ce11eac48b115a91033604ad248d04b31f677c41
nicolascordoba1 <nicolascordoba97@hotmail.com>
2022-03-05 22:02:36 -0500

    Cleaner solution to the excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,15 @@
 const sumAll = function(a,b) {
-    let suma = 0
-    let lista = [a,b]
-    lista.sort()
-
-    let prueba1 = typeof a
-    let prueba2 = typeof b
-
-    
-    for(let j=0;j<=lista[1] -lista[0] ;j++){
-        suma += lista[1] -j
-    } 
-    
-    if(suma<0 || prueba1 != 'number' || prueba2 != 'number' ){
-        return 'ERROR'
-    } else{
-        return suma
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR'
+    if(a<0 || b<0) return 'ERROR'
+    if(a>b){
+        let temp = b
+        b = a
+        a = temp
     }
+    let suma = 0
+    for(i=a; i<=b; i++){
+        suma +=i
+    } return suma
 
 };
 
