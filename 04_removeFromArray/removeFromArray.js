a94f409296016749437de9514f35edd673c83d41
nicolascordoba1 <nicolascordoba97@hotmail.com>
2022-03-05 20:58:19 -0500

    Excercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,12 @@
-const removeFromArray = function(argumentos) {
-    let lista = []
-    for(let i=0; i<argumentos[0].length;i++){
-        for (let j=1;j<i;j++){
-          if(lista[i] === argumentos[j]){
-            lista.splice(i,1)
-            }  
-        }      
-    }
-    return lista
+const removeFromArray = function(lista, ...numeros) {
+    numeros.sort()
+    for(let i=0; i<=lista.length; i++){
+        for(let j=0; j<numeros.length;j++){
+            if(lista[i]===numeros[j]){
+                lista.splice(i,1)
+            }
+        }
+    } return lista
 };
 
 // Do not edit below this line
