67c44a7a8f7a01b7d54a23bcd8343ef27b8e7cf8
nicolascordoba1 <nicolascordoba97@hotmail.com>
2022-03-05 21:48:15 -0500

    Excercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let suma = 0
+    let lista = [a,b]
+    lista.sort()
+
+    let prueba1 = typeof a
+    let prueba2 = typeof b
+
+    
+    for(let j=0;j<=lista[1] -lista[0] ;j++){
+        suma += lista[1] -j
+    } 
+    
+    if(suma<0 || prueba1 != 'number' || prueba2 != 'number' ){
+        return 'ERROR'
+    } else{
+        return suma
+    }
 
 };
 
