0ef99c16865c87d5f8b17b68268019fe1c17377d
nicolascordoba1 <nicolascordoba97@hotmail.com>
2022-03-07 15:02:16 -0500

    Exercises completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  const tempc = (tempf - 32)*(5/9)
+  if(tempc==0){
+    return tempc
+  } return parseFloat(tempc.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  const tempf = (tempc * (9/5)) + 32
+  if(tempf == 0){
+    return tempf
+  }return parseFloat(tempf.toFixed(1))
 };
 
 // Do not edit below this line
